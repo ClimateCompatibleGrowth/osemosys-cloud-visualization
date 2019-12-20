1a0093b5170af33df2757eefe1dd8a7b7b9e9c7a
Youssef Boulkaid <yboulkaid@gmail.com>
2019-12-21 00:06:46 +0100

    Use tmp folder for files

--- app/setup.py
+++ app/setup.py
@@ -5,8 +5,8 @@ from zipfile import ZipFile
 
 def download_files(url):
     random_number = random.randint(1,99999)
-    zip_file_name = f'csv_{random_number}.zip'
-    folder_name = f'csv_{random_number}'
+    zip_file_name = f'tmp/csv_{random_number}.zip'
+    folder_name = f'tmp/csv_{random_number}'
     wget.download(url, zip_file_name)
     zip_path = os.path.join(os.getcwd(), zip_file_name)
     with ZipFile(zip_path, 'r') as zipObj:
