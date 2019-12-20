0796b04d79bcdb20c054d144699a68a919b23dc8
Youssef Boulkaid <yboulkaid@gmail.com>
2019-12-21 00:07:56 +0100

    Cleanup code

--- app.py
+++ app.py
@@ -20,21 +20,6 @@ from utilities import df_plot, df_filter
 from calculations import calculate_cap_df, calculate_gen_df, calculate_gen_use_df, calculate_cap_cos_df, calculate_ene_imp_df, calculate_ene_exp_df, calculate_dom_prd_df
 from figures import fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, fig9
 
-name_color_codes = pd.read_csv(os.path.join(os.getcwd(),'name_color_codes.csv'), encoding='latin-1')
-det_col = dict([(c,n) for c,n in zip(name_color_codes.code, name_color_codes.name_english)])
-
-# List of columns for aggregated energy tables and figures
-agg_col = {'Coal':['Coal'],
-        'Oil': ['Diesel','HFO','JFL','Crude oil','Petroleum coke'],
-        'Gas': ['Natural gas','LNG','LPG'],
-        'Hydro': ['Hydro'],
-        'Nuclear': ['Nuclear'],
-        'Other renewables': ['Biomass','Geothermal','Solar','MSW','Wind'],
-        'Net electricity imports': ['Net electricity imports']
-        }
-
-
-
 def setup_app(url):
     all_figures = {}
 
