a73c61c023f82617eaf6d93e059faf30996796a9
Youssef Boulkaid <yboulkaid@gmail.com>
2020-01-18 13:27:07 +0100

    Add possibility to chose model

--- app/setup.py
+++ app/setup.py
@@ -3,6 +3,25 @@ import wget
 import os
 from zipfile import ZipFile
 
+def input_path(url):
+    if url == 'bolivia':
+        return bolivia_files()
+    elif url == 'ethiopia':
+        return ethiopia_files()
+    elif url == 'vietnam':
+        return vietnam_files()
+    else:
+        return download_files(url)
+
+def ethiopia_files():
+    return os.path.join(os.getcwd(), 'data/ethiopia/csv/')
+
+def bolivia_files():
+    return os.path.join(os.getcwd(), 'data/bolivia/csv/')
+
+def vietnam_files():
+    return os.path.join(os.getcwd(), 'data/vietnam/csv/')
+
 def download_files(url):
     random_number = random.randint(1,99999)
     zip_file_name = f'tmp/csv_{random_number}.zip'
@@ -12,6 +31,3 @@ def download_files(url):
     with ZipFile(zip_path, 'r') as zipObj:
         zipObj.extractall(folder_name)
     return os.path.join(os.getcwd(), f'{folder_name}/csv/')
-
-def vietnam_files():
-    return os.path.join(os.getcwd(), 'data/vietnam/csv/')
