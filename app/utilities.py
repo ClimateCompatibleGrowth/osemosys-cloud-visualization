b2a154e4afbfe441677b84be9fad7b85f873924b
Youssef Boulkaid <yboulkaid@gmail.com>
2020-03-14 19:15:46 +0100

    Move data to indoniesia folder

--- app/utilities.py
+++ app/utilities.py
@@ -7,14 +7,14 @@ det_col = dict([(c,n) for c,n in zip(name_color_codes.code, name_color_codes.nam
 def landuse():
     technologies = []
     commodities = []
-    with open(os.path.join(os.getcwd(),'data.txt'), 'r') as f:
+    with open(os.path.join(os.getcwd(), 'data', 'indonesia', 'data.txt'), 'r') as f:
         for line in f:
             if line.startswith(('set TECHNOLOGY')):
                 technologies = line.split(' ')[3:]
             if line.startswith(('set COMMODITY', 'set FUEL')):
                 commodities = line.split(' ')[3:]
 
-    # Construct dictionary mapping modes to crop combos {mode:crop_combo}    
+    # Construct dictionary mapping modes to crop combos {mode:crop_combo}
     crop_list = sorted(list(set([x[1:5] for x in commodities if x.startswith('LCP')])))
     crop_order = ['HI','II','HR','IR','LR']
     crop_combo = []
@@ -24,19 +24,19 @@ def landuse():
             crop_combo.append(each_crop + each_combo)
 
     for each in ['BAR','FOR','GRS','BLT','WAT','OTH']:
-        crop_combo.append(each)      
-    
+        crop_combo.append(each)
+
     mode_crop_combo = dict([(m,c) for m,c in zip(range(1,len(crop_combo)+1),crop_combo)])
 
-    # Construct dictionary of regions {region_code:region_name}. 
+    # Construct dictionary of regions {region_code:region_name}.
     # Region codes are extracted from the data file
     regions_list = sorted(list(set([x[6:-3] for x in technologies if x.startswith('LNDAGR')])))
     regions = {}
 
     for each in regions_list:
         regions[each] = each
-    
-    # Construct dictionary of crops {crop_code:crop_name}. 
+
+    # Construct dictionary of crops {crop_code:crop_name}.
     # Crop codes and names are extracted from the 'name_color_codes.csv' file
     crops = {}
     for each in det_col.keys():
