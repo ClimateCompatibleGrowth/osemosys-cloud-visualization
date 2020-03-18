3238dc49c82c50f718cce72af8a13278abef6e94
Youssef Boulkaid <yboulkaid@gmail.com>
2020-03-18 22:11:47 +0100

    Test it out

---
+++ app/test.py
@@ -0,0 +1,42 @@
+import random
+import wget
+import os
+from zipfile import ZipFile
+
+def input_path(url):
+    if url == 'bolivia':
+        return bolivia_files()
+    elif url == 'ethiopia':
+        return ethiopia_files()
+    elif url == 'vietnam':
+        return vietnam_files()
+    elif url == 'indonesia':
+        return indonesia_files()
+    else:
+        return download_files(url)
+
+def ethiopia_files():
+    return os.path.join(os.getcwd(), 'data', 'ethiopia', 'csv')
+
+def bolivia_files():
+    return os.path.join(os.getcwd(), 'data', 'bolivia', 'csv')
+
+def vietnam_files():
+    return os.path.join(os.getcwd(), 'data', 'vietnam', 'csv')
+
+def indonesia_files():
+    return os.path.join(os.getcwd(), 'data', 'indonesia', 'csv')
+
+def download_files(url):
+    random_number = random.randint(1,99999)
+    zip_file_name = f'csv_{random_number}.zip'
+    folder_name = f'csv_{random_number}'
+
+    zip_path = os.path.join(os.getcwd(), 'tmp', zip_file_name)
+    wget.download(url, zip_path)
+    folder_path = os.path.join(os.getcwd(), 'tmp', folder_name)
+
+    with ZipFile(zip_path, 'r') as zipObj:
+        zipObj.extractall(folder_path)
+
+    return os.path.join(os.getcwd(), 'tmp', folder_name, 'csv')
