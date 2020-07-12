191a16204db9ab00a8a7e7440a58786217b08a3c
Youssef Boulkaid <yboulkaid@gmail.com>
2020-07-12 10:08:24 +0200

    Make config.py more modular

--- app/config.py
+++ app/config.py
@@ -9,20 +9,17 @@ class Config:
         self.input_string = input_string
 
     def csv_folder_path(self):
-        return self.__input_path()
+        return os.path.join(self.__base_folder_path(), 'csv')
 
     def data_file_path(self):
-        # self.input_string only works locally for now
-        return os.path.join(os.getcwd(), 'data', self.input_string, 'data.txt')
+        return os.path.join(self.__base_folder_path(), 'data.txt')
 
-    def __input_path(self):
+    def __base_folder_path(self):
         if self.input_string in ['bolivia', 'ethiopia', 'vietnam', 'indonesia']:
-            return self.__local_csv_folder_for(self.input_string)
+            return os.path.join(os.getcwd(), 'data', self.input_string)
         else:
-            return __download_files(self.input_string)
-
-    def __local_csv_folder_for(self, model_name):
-        return os.path.join(os.getcwd(), 'data', model_name, 'csv')
+            raise NameError('NoFolderFound')
+            # return __download_files(self.input_string)
 
     def __download_files(self, input_string):
         random_number = random.randint(1, 99999)
