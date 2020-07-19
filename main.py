51b2f145acd8c5015f6c107b91399f98cef4ecb8
Youssef Boulkaid <yboulkaid@gmail.com>
2020-07-19 14:58:12 +0200

    Add bootstrap

--- main.py
+++ main.py
@@ -14,7 +14,37 @@ from app.generate_figures import generate_figures  # noqa
 cufflinks.go_offline()
 cufflinks.set_config_file(world_readable=True, theme='white')
 
-app = dash.Dash(__name__)
+external_scripts = [
+        {
+            'src': 'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+            'integrity': 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj',
+            'crossorigin': 'anonymous'
+        },
+        {
+            'src': 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
+            'integrity': 'sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo',
+            'crossorigin': 'anonymous'
+        },
+        {
+            'src': 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js',
+            'integrity': 'sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI',
+            'crossorigin': 'anonymous'
+        }
+    ]
+
+external_stylesheets = [
+        {
+            'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+            'rel': 'stylesheet',
+            'integrity': 'sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk',
+            'crossorigin': 'anonymous'
+        }
+    ]
+
+app = dash.Dash(__name__,
+                external_scripts=external_scripts,
+                external_stylesheets=external_stylesheets)
+
 server = app.server
 
 app.layout = html.Div([
