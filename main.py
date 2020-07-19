8a8e1f2412b84d3667c298c00a3c84a67064a9ad
Youssef Boulkaid <yboulkaid@gmail.com>
2020-07-19 13:50:07 +0200

    Move css to file

--- main.py
+++ main.py
@@ -29,20 +29,9 @@ app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
         children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
+            html.Button(children='Upload files'),
         ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
+        className='upload-zone',
         multiple=True
     ),
 
