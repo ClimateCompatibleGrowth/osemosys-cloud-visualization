1d8ef1bb75c9e83a64d8b0523419448a43bce2f2
Youssef Boulkaid <yboulkaid@gmail.com>
2020-07-26 15:48:11 +0200

    Add id to figures

--- app/dash_figure.py
+++ app/dash_figure.py
@@ -1,10 +1,14 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import random
 
 
 class DashFigure:
     def __init__(self, iplot):
         self.iplot = iplot
 
-    def to_div(self):
-        return html.Div(dcc.Graph(figure=self.iplot), className='figure')
+    def to_div(self, id=''):
+        random_number = random.randint(1, 99999)
+        if id is '':
+            id = random_number
+        return html.Div(dcc.Graph(figure=self.iplot), className=f'figure figure-{id}')
