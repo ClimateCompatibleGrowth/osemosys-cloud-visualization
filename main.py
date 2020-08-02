41bb88beca56b5ab9c58db89d3b350e6747d251d
Youssef Boulkaid <yboulkaid@gmail.com>
2020-08-02 15:29:49 +0200

    Add title metadata

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ server = app.server
 
 app.layout = html.Div([
     dcc.Location(id='url'),
-    html.H1('CLEWS Dashboard'),
+    html.H1('CLEWS Dashboard', id='title'),
     html.Div('An interactive tool to visualise CLEWS model results', className='subtitle'),
     html.Div([
             dcc.Input(id='input-string', type='text', className='input-field'),
@@ -114,6 +114,26 @@ def populate_input_string_from_query_string(query_string):
 
 
 @app.callback(
+    Output(component_id='title', component_property='children'),
+    [
+        Input(component_id='submit-button', component_property='n_clicks'),
+        Input(component_id='input-string', component_property='n_submit'),
+        Input(component_id='url', component_property='search'),
+        Input(component_id='upload-data', component_property='contents'),
+    ],
+    [State('input-string', 'value')]
+    )
+def generate_figure_divs(n_clicks, n_submit, raw_query_string, upload_data, input_string):
+    triggered_element = dash.callback_context.triggered[0]['prop_id']
+    config_input = config_input_from(input_string, raw_query_string, triggered_element)
+    config = Config(config_input)
+    if config.is_valid():
+        return config.title()
+    else:
+        return 'CLEWS Dashboard'
+
+
+@app.callback(
     Output(component_id='figures-container', component_property='children'),
     [
         Input(component_id='submit-button', component_property='n_clicks'),
@@ -124,23 +144,28 @@ def populate_input_string_from_query_string(query_string):
     [State('input-string', 'value')]
     )
 def generate_figure_divs(n_clicks, n_submit, raw_query_string, upload_data, input_string):
-    config_input = input_string
-
-    if input_string is None and raw_query_string is None:
-        return []
-
-    if input_string is None and raw_query_string is not None:  # First initialization
-        config_input = parse_query_string(raw_query_string)
-
     triggered_element = dash.callback_context.triggered[0]['prop_id']
-    if triggered_element in ['upload-data.contents']:
-        config_input = process_uploaded_file(upload_data)
-
+    config_input = config_input_from(input_string, raw_query_string, triggered_element)
     config = Config(config_input)
     if config.is_valid():
         return GenerateDivs(config).generate_divs()
     else:
         return [f'Invalid model: {config_input}']
+
+
+def config_input_from(input_string, raw_query_string, triggered_element=''):
+    config_input = input_string
+
+    if input_string is None and raw_query_string is None:
+        return ''
+
+    if input_string is None and raw_query_string is not None:  # First initialization
+        config_input = parse_query_string(raw_query_string)
+
+    if triggered_element in ['upload-data.contents']:
+        config_input = process_uploaded_file(upload_data)
+
+    return config_input
 
 
 def parse_query_string(query_string):
