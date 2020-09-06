bd4d48e12a7cea298886b300db9962fd0d64a133
Youssef Boulkaid <yboulkaid@gmail.com>
2020-09-06 10:54:56 +0200

    Style errors too

--- app/dash_figure_set.py
+++ app/dash_figure_set.py
@@ -14,23 +14,26 @@ class DashFigureSet:
     @functools.lru_cache(maxsize=128)
     def to_div(self):
         print(f'Generating {self.name}')
+        return html.Div(
+                    [
+                        html.H4(self.name),
+                        self.__content()
+                    ],
+                    className=f'figure-set figure-set-{self.id}',
+                )
+
+    def __content(self):
         try:
             return html.Div(
-                        [
-                            html.H4(self.name),
-                            html.Div(
-                                [
-                                    html.Div(dcc.Graph(figure=iplot.figure()), className='figure')
-                                    for iplot in self.iplots
-                                ],
-                                className='figures-in-set-container'
-                            )
-                        ],
-                        className=f'figure-set figure-set-{self.id}',
-                    )
+                [
+                    html.Div(dcc.Graph(figure=iplot.figure()), className='figure')
+                    for iplot in self.iplots
+                ],
+                className='figures-in-set-container'
+            )
         except Exception as e:
             return html.Div([
                     html.Pre(traceback.format_exc(), className='card-body'),
                 ],
-                className=f'figure-set-error card figure-set-{self.id}'
+                className=f'figure figure-error card'
             )
