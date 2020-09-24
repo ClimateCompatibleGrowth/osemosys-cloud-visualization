872db92dba2e8df9c8d1b7c8b38c58c3ba55eb14
Youssef Boulkaid <yboulkaid@gmail.com>
2020-09-24 13:37:21 +0200

    Use venv_asdf

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,7 @@
 set -xue
 
 git pull
-source venv/bin/activate
+source venv_asdf/bin/activate
 pip3 install -r requirements.txt
 rm -rf cache
 sudo systemctl restart osemosys-cloud-visualization.service
